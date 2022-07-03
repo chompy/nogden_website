202e31118b295b1f85c6e68b299c97dfe7df006c
Nathan Ogden <nathan@ogden.tech>
2022-07-03 03:31:17 -0400

    built mini site generator, minify css, use shorted tags, ids, classes, images paths, etc; resize and crop thumbnails and embed with base64

--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.16
 
 require (
 	github.com/gomarkdown/markdown v0.0.0-20210514010506-3b9f47219fe7 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/oliamb/cutter v0.2.2 // indirect
+	github.com/tdewolff/minify/v2 v2.11.11 // indirect
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
 	"html/template"
+	"image"
+	"image/jpeg"
+	_ "image/jpeg"
+	"image/png"
+	_ "image/png"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gomarkdown/markdown"
+	"github.com/nfnt/resize"
+	"github.com/oliamb/cutter"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/css"
+	htmlMini "github.com/tdewolff/minify/v2/html"
+	"golang.org/x/net/html"
 
 	"gopkg.in/yaml.v2"
 )
+
+const imgThumbWidth = 615
+const imgJpgQuality = 50
 
 type Site struct {
 	Info struct {
@@ -47,16 +65,78 @@ func main() {
 		panic(err)
 	}
 
-	// load template
+	// load template + generate html
 	t := template.New("site.tmpl").Funcs(template.FuncMap{
 		"Markdown": func(v string) template.HTML {
 			return template.HTML(markdown.ToHTML([]byte(v), nil, nil))
 		},
 	})
 	t = template.Must(t.ParseFiles("data/site.tmpl"))
-
-	if err := t.Execute(os.Stdout, site); err != nil {
+	buf := bytes.Buffer{}
+	if err := t.Execute(&buf, site); err != nil {
 		panic(err)
 	}
+
+	// crawl html look for images, resize as needed
+	outputHtml := buf.String()
+	doc, err := html.Parse(bytes.NewReader(buf.Bytes()))
+	if err != nil {
+		panic(err)
+	}
+	var crawler func(*html.Node)
+	crawler = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "img" {
+			src := ""
+			for _, a := range node.Attr {
+				if a.Key == "src" {
+					src = a.Val
+				}
+			}
+			imgFile, err := os.Open(src)
+			if err != nil {
+				panic(err)
+			}
+			img, _, err := image.Decode(imgFile)
+			if err != nil {
+				panic(err)
+			}
+			imgBuf := bytes.Buffer{}
+			imgType := "jpeg"
+			if img.Bounds().Size().X > imgThumbWidth {
+				img = resize.Resize(imgThumbWidth, 0, img, resize.Lanczos3)
+				img, err = cutter.Crop(img, cutter.Config{
+					Width:   16,
+					Height:  9,
+					Mode:    cutter.Centered,
+					Options: cutter.Ratio,
+				})
+				if err := jpeg.Encode(&imgBuf, img, &jpeg.Options{Quality: imgJpgQuality}); err != nil {
+					panic(err)
+				}
+			} else {
+				imgType = "png"
+				pngEnc := png.Encoder{CompressionLevel: png.BestCompression}
+				if err := pngEnc.Encode(&imgBuf, img); err != nil {
+					panic(err)
+				}
+			}
+			b64Img := base64.StdEncoding.EncodeToString(imgBuf.Bytes())
+			outputHtml = strings.ReplaceAll(
+				outputHtml,
+				fmt.Sprintf("img src=\"%s\"", src),
+				fmt.Sprintf("img src=\"data:image/%s;base64,%s\"", imgType, b64Img),
+			)
+			return
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			crawler(child)
+		}
+	}
+	crawler(doc)
+
+	m := minify.New()
+	m.AddFunc("text/html", htmlMini.Minify)
+	m.AddFunc("text/css", css.Minify)
+	m.Minify("text/html", os.Stdout, bytes.NewReader([]byte(outputHtml)))
 
 }
